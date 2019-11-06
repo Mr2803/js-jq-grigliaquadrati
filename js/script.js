4ee205b23f0fe8328622f5baf6059b350734f2b0
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-06 14:19:19 +0100

    delete first script (left only the best solution for basic exercise)

--- js/script.js
+++ js/script.js
@@ -2,19 +2,26 @@ $(document).ready(function () {
 
     var punteggio = 0;
     var inner = document.getElementById("punteggio");
-$(".red").click(function (){
-    $(this).css("background" , "red");
     
-    punteggio ++;
-    inner.innerHTML =  punteggio;
-});
+    $(".grid-item").click(function () {
 
-$(". ").click(function (){
-    $(this).css("background" , " ")
-});
+       var redOrNot =  $(this);
+
+        if (redOrNot.hasClass("red")){
+            redOrNot.css("background", "red");
+            punteggio++;
+            inner.innerHTML = punteggio;
+       } else{
+            redOrNot.css("background", "green ")
+       }
+
+       
+    });
+
+   
 
 
-/* console.log(punteggio) */
+    /* console.log(punteggio) */
 
 
 
