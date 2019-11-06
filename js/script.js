cffb626bbf94386fc757f15e39715d57d663bdac
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-06 13:34:38 +0100

    finish basic exercise

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 
-
+$(".red").click(function (){
+    $(this).css("background" , "red")
+})
+$(".green").click(function (){
+    $(this).css("background" , "green")
+})
 
 
 
