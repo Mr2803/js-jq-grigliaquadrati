7d9d8fa1af2d40191d40105332ebb415c224c503
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-06 18:14:08 +0100

    add alert for the winners or losers

--- bonus/script2.js
+++ bonus/script2.js
@@ -12,6 +12,9 @@ $(document).ready(function () {
 
     console.log($(".grid-item"))
     //tramite jquery imposto la mia funzione che si attiva tramite click
+    
+
+    
     $(".grid-item").click(function () {
         //assegno ad una variabile il valore $(this) in modo tale che si azioni sul singolo quadrato
 
@@ -19,17 +22,25 @@ $(document).ready(function () {
         
        //imposto la condizione per generare il colore dei quadrati random , con la condizione che
        //se il punteggio dei rossi è più alto di 14 allora non verranno più generati rossi
-            if(punteggioR > 14){
-                $(this).addClass("green")
-            }else{
+            if(punteggioR >= 14){
+                $(this).addClass("green");
+                if(punteggioG <= 20){
+                    if (!alert('Complimenti hai vinto, premi OK per un\'altra partita')) { 
+                        window.location.reload(); }
+                }
+            } else{
                 $(this).addClass(classes[Math.floor(Math.random() * classes.length)]);
-            }
                 
+                if(punteggioG >= 19){
+                    if (!alert('Mi dispiace hai perso , premi OK per un\'altra partita')) { window.location.reload(); }
+                }
+            }
+        console.log("questa è la lunghezza di classes.length" + classes.length)     
             
    
        //imposto la condizione SE ha classe RED allora ...
         if (redOrNot.hasClass("red")){
-            redOrNot.css("background", "red").fadeIn(3000).fadeOut(2000)/* .fadeOut(1000).fadeIn() */;
+            redOrNot.css("background", "red")/* .fadeOut(1000).fadeIn() */;
                 redOrNot.off();
                 punteggioR++;
                 inner.innerHTML = punteggioR;
@@ -41,7 +52,6 @@ $(document).ready(function () {
                         "font-size", "100%")
                 }); */
                 punteggioG++;
-                console.log(punteggioG)
                 inner2.innerHTML = punteggioG;
                 redOrNot.off();
        }
@@ -49,7 +59,7 @@ $(document).ready(function () {
        
     });
 
-   
+
 
 
     /* console.log(punteggio) */
