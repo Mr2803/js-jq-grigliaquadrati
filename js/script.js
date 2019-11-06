636a564c2bb2dc35a1a1ae09742dea4c2d234bfd
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-06 14:24:06 +0100

    add comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
 
+    //imposto una var punteggio a 0 che aumenta se viene trovato un quadratino rosso
     var punteggio = 0;
     var inner = document.getElementById("punteggio");
     
+    //tramite jquery imposto la mia funzione che si attiva tramite click
     $(".grid-item").click(function () {
 
+        //assegno ad una variabile il valore $(this) in modo tale che si azioni sul singolo quadrato
        var redOrNot =  $(this);
 
+       //imposto la condizione SE ha classe RED allora ...
         if (redOrNot.hasClass("red")){
             redOrNot.css("background", "red");
             punteggio++;
             inner.innerHTML = punteggio;
+        //altrimenti ...
        } else{
             redOrNot.css("background", "green ")
        }
