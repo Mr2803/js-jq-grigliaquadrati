04893037b0a686c6ed7ace074936dbbff274a630
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-06 13:40:27 +0100

    create a var for score

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 
+    var punteggio = 0;
+
 $(".red").click(function (){
     $(this).css("background" , "red")
-})
+    
+    punteggio ++;
+    console.log(punteggio)
+});
+
 $(".green").click(function (){
     $(this).css("background" , "green")
-})
+});
 
 
-
+/* console.log(punteggio) */
 
 
 
